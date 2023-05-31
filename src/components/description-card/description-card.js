2cd91f597b50868e1aaf3b590c9e6cd3085290ee
badmbadn <kornuskaevbadma@gmail.com>
2023-05-31 16:56:09 +0300

    finally commit

--- src/components/description-card/description-card.js
+++ src/components/description-card/description-card.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import './description-card.css';
 
-function cuttingString(str) {
-  const text = str.slice(0, 100);
-  const a = text.split(' ');
-  a.splice(a.length - 1, 1);
-  const res = a.join(' ');
-  return `${res}...`;
-}
+import { cuttingString } from '../../utilitary/cuttingString';
+import './description-card.css';
 
 function DescriptionCard({ overview }) {
   return (
--- src/components/movie-title/movie-title.js
+++ src/components/movie-title/movie-title.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { format } from 'date-fns';
+
+import { colorRating } from '../../utilitary/colorRating';
 import './movie-title.css';
 
 function MovieTitle({ title, releaseDate, voteAverage }) {
-  function colorRating(number) {
-    const raiting = ['rating'];
-
-    if (number < 3) {
-      raiting.push('cirrcleColor1');
-    } else if (number >= 3 && number < 5) {
-      raiting.push('cirrcleColor2');
-    } else if (number >= 5 && number < 7) {
-      raiting.push('cirrcleColor3');
-    } else {
-      raiting.push('cirrcleColor4');
-    }
-
-    return raiting;
-  }
   return (
     <>
       <h5 className="card-movie__title">{title}</h5>
